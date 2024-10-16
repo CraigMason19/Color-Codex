2e942287512ce92086adc3eb29a0e0cef25ca180
SadHappy <craigjm19@gmail.com>
2024-10-16 18:57:18 +0100

    Added tests for Color.fromRGB Method

--- scripts/tests/color.test.js
+++ scripts/tests/color.test.js
@@ -9,3 +9,14 @@ describe('color construction', () => {
         expect(color.data).toEqual([255, 255, 0]);
     });
 });
+
+describe('fromRGB', () => {
+    test('clamp values out of upper range', () => {
+        let color = Color.fromRGB(255, 256, 4000);
+        expect(color.data).toEqual([255, 255, 255]);
+    });
+    test('clamp values out of lower range', () => {
+        let color = Color.fromRGB(0, -5, -4000);
+        expect(color.data).toEqual([0, 0, 0]);
+    });
+});

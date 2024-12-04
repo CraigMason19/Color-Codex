6aec8109b10fcc3eedb24c54b903beb4408ca540
SadHappy <craigjm19@gmail.com>
2024-12-04 16:19:32 +0000

    Added ability to pass 0 to popup fadeOutTime to keep it from disappearing

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,7 +2,15 @@ import { Color } from './color.js';
 
 const POPUP_FADE_OUT = 6000;
 
-// Function to show popup with fade out effect
+/**
+ * Displays a colored popup with a title and content.
+ *
+ * @param {string} title - The title to be displayed in the popup.
+ * @param {string} content - The content to be displayed in the popup.
+ * @param {Color} color - An instance of the `Color` class representing the popup's background color.
+ * @param {number} [fadeOutTime=6000] - The time in milliseconds to display the popup before fading out.
+ *                                      Pass `0` to keep the popup visible until another popup is created.
+ */
 export function showPopup(title, content, color, fadeOutTime=POPUP_FADE_OUT) {
     const popup = document.getElementById('popup');
     const popupContent = document.getElementById('popup-content');
@@ -14,7 +22,9 @@ export function showPopup(title, content, color, fadeOutTime=POPUP_FADE_OUT) {
 
     popup.classList.add('show');
 
-    setTimeout(() => {
-        popup.classList.remove('show');
-    }, fadeOutTime);
+    if(fadeOutTime !== 0) {
+        setTimeout(() => {
+            popup.classList.remove('show');
+        }, fadeOutTime);
+    }
 }
